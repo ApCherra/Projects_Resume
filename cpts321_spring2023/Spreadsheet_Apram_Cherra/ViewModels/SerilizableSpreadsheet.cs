33328f3a78df7cff6dac4140c4cbee78582f2127
ApCherra <135000565+ApCherra@users.noreply.github.com>
2023-05-30 20:37:34 -0700

    Delete cpts321_spring2023 directory

--- cpts321_spring2023/Spreadsheet_Apram_Cherra/ViewModels/SerilizableSpreadsheet.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using SpreadsheetEngine;
-
-namespace Spreadsheet_Apram_Cherra.ViewModels;
-
-public class SerilizableSpreadsheet
-{
-    public List<Cell> cells = new List<Cell>();
-}
