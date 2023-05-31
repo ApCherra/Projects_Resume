840eb3b5df33e4bcd90bdd49a05ce25806bcaad8
ApCherra <135000565+ApCherra@users.noreply.github.com>
2023-05-30 20:56:31 -0700

    Delete cpts321_spring2023 directory

--- cpts321_spring2023/HW5/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using HW5;
-
-Menu m = new Menu();
-m.MenuInterface();
--- cpts321_spring2023/Spreadsheet_Apram_Cherra/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace Spreadsheet_Apram_Cherra.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
